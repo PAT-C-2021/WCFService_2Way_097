8201eb9574714eec7f6d80ce6cc1d435be40711d
ffadelm <faysalfadelm0107@gmail.com>
2022-01-06 16:43:41 +0700

    Add project files.

---
+++ IClientCallBack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCFService_2Way_20190140097
+{
+    [ServiceContract]
+    interface IClientCallBack
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesanKirim(string user, string pesan);
+    }
+}
---
+++ ServiceCallBack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_20190140097
+{
+    [ServiceBehavior(ConcurrencyMode= ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.Single)]
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ServiceCallBack : IServiceCallBack
+    {
+        //menyimpan data saat user online
+        Dictionary<IClientCallBack, string> userList = new Dictionary<IClientCallBack, string>();
+        public void gabung(string username)
+        {
+            //menampung user saat baru daftar
+            IClientCallBack koneksiGabung = OperationContext.Current.GetCallbackChannel<IClientCallBack>();
+            userList[koneksiGabung] = username;
+        }
+
+        public void kirimPesan(string pesan)
+        {
+            //mengirim data user dan pesan ke userlain
+            IClientCallBack koneksiPesan = OperationContext.Current.GetCallbackChannel<IClientCallBack>();
+            string user;
+            if(!userList.TryGetValue(koneksiPesan, out user))
+            {
+                return;
+            }
+            foreach(IClientCallBack other in userList.Keys)
+            {
+                other.pesanKirim(user, pesan);
+            }
+        }
+    }
+}
